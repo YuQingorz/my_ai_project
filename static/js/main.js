b2ac4c84e020922eaad953da6f55785859c9ecc8
YuQingorz <75086282+YuQingorz@users.noreply.github.com>
2023-05-14 10:34:43 +0800

    fix

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,12 @@
 particlesJS.load("particles-js", "/static/particles.json", function () {
   console.log("callback - particles.js config loaded");
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+  const polygonCanvas = document.querySelector('.particles-canvas-el');
+  if (polygonCanvas) {
+    polygonCanvas.style.position = 'absolute';
+    polygonCanvas.style.bottom = '0';
+    polygonCanvas.style.zIndex = '-2';
+  }
+});
